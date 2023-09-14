db55a770c373b8acc3200c3fbc40bb291ee2f585
sarah <sarah.peng@berkeley.edu>
2023-09-14 15:46:48 -0700

    Initialize repo

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="box"> 
+        <div>Welcome Back!</div>
+        <div>Sign in to continue</div>
+        
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
